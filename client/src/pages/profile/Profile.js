fccd139b7a79a46c5dd9fc78d58466cca9e0b898
jeremiah <ojoj665@gmail,com>
2023-01-27 20:48:45 +0100

    configure profile

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,30 +1,79 @@
 import "./Profile.scss";
 import Card from "../../components/card/Card";
 import profimg from "../../assets/avatarr.png";
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import PageMenu from "../../components/pageMenu/PageMenu";
+import UseRedirectSession from "../../customHook/UseRedirectSession";
+import { useSelector, useDispatch } from "react-redux"
+import { getUser } from "../../redux/features/auth/authSlice";
+import Loader from "../../components/loading/Loader";
+import { toast } from "react-toastify";
 
 
-const initialState = {
-    name:"jerry",
-    email:"ojoj665@gmail.com",
-    phone:"",
-    bio:"",
-    photo:"",
-    role:"",
-    isverified:"false",
-}
+const cloudName = process.env.CLOUD_NAME
+const uploadPreset = process.env.UPLOAD_PRESET
+
+
 const Profile = () => {
-    const [profile, setProfile] = useState(initialState)
-    const handleImageChange =()=>{
+  UseRedirectSession("/login")
+    
+    const dispatch = useDispatch()
+    const {isLoading, isLoggedin, isSuccess, message, user} = useSelector((state)=> state.auth)
 
+    const initialState = {
+      name:user?.name || "",
+      email:user?.email || "",
+      phone:user?.phone || "",
+      bio:user?.bio || "",
+      photo:user?.photo || "",
+      role:user?.role || "",
+      isverified:user?.isVerified || false,
+  }
+  const [profile, setProfile] = useState(initialState)
+  const [profileImg, setProfileImg] = useState(null)
+  const [imagePreview, setImagePreview] = useState(null)
+
+  useEffect(()=>{
+    dispatch(getUser())
+  },[dispatch])
+
+
+    const handleImageChange =(e)=>{
+      setProfileImg(e.target.files[0])
+      setImagePreview(URL.createObjectURL(e.target.files[0]))
     }
-    const handleInputChange =()=>{
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setProfile({ ...profile, [name]: value });
+    };
+
+    const saveProfile =async(e)=>{
+      e.preventDefault()
+      let imageURL;
+      try{
+        if(profileImg !== null && (
+          profileImg.type === "image/jpeg" || 
+          profileImg.type === "image/jpg" || 
+          profileImg.type === "image/png")){
+            const image = new FormData()
+            image.append("file", profileImg)
+            image.append("cloud_name", cloudName)
+            image.append("upload_preset", uploadPreset)
+
+            // save image to cloudinary
+            const response = await fetch("https://api.cloudinary.com/v1_1/blazejay/image/upload", {method: "POST", body:image})
+            const imgData = await response.json()
+            imageURL= imgData.url.toString()
+          }
+      }catch(err){
+        toast.error(err.message)
+      }
 
     }
   return (
     <>
       <section className="container">
+        {isLoading && <Loader/>}
         <PageMenu/>
         <h2>Profile</h2>
         <div className="--flex-start profile">
@@ -32,11 +81,11 @@ const Profile = () => {
             <>
               <div className="profile-photo">
                 <div>
-                  <img src={profimg} alt="profile pic" />
-                  <h3>Role:Subscriber</h3>
+                  <img src={imagePreview === null ? user?.photo : imagePreview} alt="profile pic" />
+                  <h3>Role: {profile?.role}</h3>
                 </div>
               </div>
-              <form>
+              <form onSubmit={saveProfile} >
                 <p>
                   <label>Change Photo:</label>
                   <input
@@ -51,7 +100,7 @@ const Profile = () => {
                   <input
                     type="text"
                     name="name"
-                    value={profile.name}
+                    value={profile?.name}
                     onChange={handleInputChange}
                   />
                 </p>
@@ -60,7 +109,7 @@ const Profile = () => {
                   <input
                     type="email"
                     name="email"
-                    value={profile.email}
+                    value={profile?.email}
                     onChange={handleInputChange}
                     disabled
                   />
@@ -70,7 +119,7 @@ const Profile = () => {
                   <input
                     type="text"
                     name="phone"
-                    value={profile.phone}
+                    value={profile?.phone}
                     onChange={handleInputChange}
                   />
                 </p>
@@ -78,7 +127,7 @@ const Profile = () => {
                   <label>Bio:</label>
                   <textarea
                     name="bio"
-                    value={profile.bio}
+                    value={profile?.bio}
                     onChange={handleInputChange}
                     cols="30"
                     rows="10"
