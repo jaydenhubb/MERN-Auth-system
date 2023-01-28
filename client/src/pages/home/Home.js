9732ed83a1438680eb991a8c338160d472343a38
jeremiah <ojoj665@gmail,com>
2023-01-28 17:29:17 +0100

    register-btn

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import "./Home.scss"
+import { Link } from "react-router-dom";
 
 import loginImg from "../../assets/login.svg"
 
@@ -13,8 +14,8 @@ const Home = () => {
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, labore?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus accusamus id optio, dignissimos reiciendis eum! </p>
           <div className="hero-button --flex-start">
-            <button className="--btn --btn-danger">Register</button>
-            <button className="--btn --btn-primary">Login</button>
+            <button className="--btn --btn-danger"><Link to="/register">Register</Link></button>
+            <button className="--btn --btn-primary"><Link to="/register">Login</Link></button>
           </div>
         </div>
         <div className="hero-image">
