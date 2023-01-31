ae0cfe6be5c46eaaebea7111925edcef62eb5200
jeremiah <ojoj665@gmail,com>
2023-01-31 10:22:14 +0100

    Profile reload bug fixed

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,91 +1,121 @@
 import "./Profile.scss";
 import Card from "../../components/card/Card";
-import profimg from "../../assets/avatarr.png";
-import {useEffect, useState} from "react"
+// import profimg from "../../assets/avatarr.png";
+import { useEffect, useLayoutEffect, useState } from "react";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import UseRedirectSession from "../../customHook/UseRedirectSession";
-import { useSelector, useDispatch } from "react-redux"
-import { getUser } from "../../redux/features/auth/authSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser, selectUser, updateProfile } from "../../redux/features/auth/authSlice";
 import Loader from "../../components/loading/Loader";
 import { toast } from "react-toastify";
 
-
-const cloudName = process.env.CLOUD_NAME
-const uploadPreset = process.env.UPLOAD_PRESET
-
-
 const Profile = () => {
-  UseRedirectSession("/login")
-    
-    const dispatch = useDispatch()
-    const {isLoading, isLoggedin, isSuccess, message, user} = useSelector((state)=> state.auth)
+  UseRedirectSession("/login");
 
-    const initialState = {
-      name:user?.name || "",
-      email:user?.email || "",
-      phone:user?.phone || "",
-      bio:user?.bio || "",
-      photo:user?.photo || "",
-      role:user?.role || "",
-      isverified:user?.isVerified || false,
-  }
-  const [profile, setProfile] = useState(initialState)
-  const [profileImg, setProfileImg] = useState(null)
-  const [imagePreview, setImagePreview] = useState(null)
+  const dispatch = useDispatch();
+  const { isLoading, isLoggedin, isSuccess, message, user } = useSelector(
+    (state) => state.auth
+  );
+  const cloudName = process.env.REACT_APP_CLOUD_NAME;
+  const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
+  const initialState = {
+    name: user?.name || "",
+    email: user?.email || "",
+    phone: user?.phone || "",
+    bio: user?.bio || "",
+    photo: user?.photo || "",
+    role: user?.role || "",
+    isverified: user?.isVerified || false,
+  };
+  const [profile, setProfile] = useState(initialState);
+  const [profileImg, setProfileImg] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
 
-  useEffect(()=>{
-    dispatch(getUser())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
 
+  const handleImageChange = (e) => {
+    setProfileImg(e.target.files[0]);
+    setImagePreview(URL.createObjectURL(e.target.files[0]));
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProfile({ ...profile, [name]: value });
+  };
 
-    const handleImageChange =(e)=>{
-      setProfileImg(e.target.files[0])
-      setImagePreview(URL.createObjectURL(e.target.files[0]))
-    }
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setProfile({ ...profile, [name]: value });
-    };
+  const saveProfile = async (e) => {
+    e.preventDefault();
+    let imageURL;
+    try {
+      if (
+        profileImg !== null &&
+        (profileImg.type === "image/jpeg" ||
+          profileImg.type === "image/jpg" ||
+          profileImg.type === "image/png")
+      ) {
+        const image = new FormData();
+        image.append("file", profileImg);
+        image.append("cloud_name", cloudName);
+        image.append("upload_preset", uploadPreset);
 
-    const saveProfile =async(e)=>{
-      e.preventDefault()
-      let imageURL;
-      try{
-        if(profileImg !== null && (
-          profileImg.type === "image/jpeg" || 
-          profileImg.type === "image/jpg" || 
-          profileImg.type === "image/png")){
-            const image = new FormData()
-            image.append("file", profileImg)
-            image.append("cloud_name", cloudName)
-            image.append("upload_preset", uploadPreset)
-
-            // save image to cloudinary
-            const response = await fetch("https://api.cloudinary.com/v1_1/blazejay/image/upload", {method: "POST", body:image})
-            const imgData = await response.json()
-            imageURL= imgData.url.toString()
-          }
-      }catch(err){
-        toast.error(err.message)
+        // save image to cloudinary
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/blazejay/image/upload",
+          { method: "POST", body: image }
+        );
+        const imgData = await response.json();
+        console.log(imgData);
+        imageURL = imgData.url.toString();
       }
 
+      // Save to database
+      const userData = {
+        name: profile.name,
+        phone: profile.phone,
+        bio: profile.bio,
+        photo: profileImg ? imageURL : profile.photo,
+      };
+      dispatch(updateProfile(userData));
+    } catch (err) {
+      toast.error(err.message);
     }
+  };
+
+  useLayoutEffect(()=>{
+    if(user){
+      setProfile({
+        ...profile, 
+        name:user.name,
+        email:user.email,
+        phone:user.phone,
+        photo:user.photo,
+        role:user.role,
+        bio:user.bio,
+        isVerified:user.isVerified,
+      })
+    }
+  },[user])
   return (
     <>
       <section className="container">
-        {isLoading && <Loader/>}
-        <PageMenu/>
+      {isLoading && <Loader />}
+        <PageMenu />
         <h2>Profile</h2>
         <div className="--flex-start profile">
           <Card cardClass={"card"}>
-            <>
+            {!isLoading && user && (
+              <>
               <div className="profile-photo">
                 <div>
-                  <img src={imagePreview === null ? user?.photo : imagePreview} alt="profile pic" />
+                  <img
+                    src={imagePreview === null ? user?.photo : imagePreview}
+                    alt="profile pic"
+                  />
                   <h3>Role: {profile?.role}</h3>
                 </div>
               </div>
-              <form onSubmit={saveProfile} >
+              <form onSubmit={saveProfile}>
                 <p>
                   <label>Change Photo:</label>
                   <input
@@ -133,14 +163,25 @@ const Profile = () => {
                     rows="10"
                   ></textarea>
                 </p>
-                <button className="--btn --btn-primary --btn-block">Update Profile</button>
+                <button className="--btn --btn-primary --btn-block">
+                  Update Profile
+                </button>
               </form>
             </>
+            )}
+            
           </Card>
         </div>
       </section>
     </>
   );
 };
+
+export const UserName = ()=>{
+  const user = useSelector(selectUser)
+  const userName = user?.name || "..."
+
+  return <p className="--color-white">Hi, {userName} |</p>
+}
 
 export default Profile;
